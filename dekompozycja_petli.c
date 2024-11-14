33a463197c0d5ac5fb9480da2d42f9d36798aae2
cPaletta <krzkaleta07@gmail.com>
2024-11-14 23:08:28 +0100

    edycja do sprawka

--- dekompozycja_petli.c
+++ dekompozycja_petli.c
@@ -29,8 +29,6 @@ double calka_zrownoleglenie_petli(double a, double b, double dx, int l_w){
   N_global = N;
 
   printf("Obliczona liczba trapezów: N = %d, dx_adjust = %lf\n", N, dx_adjust);
-  //printf("a %lf, b %lf, n %d, dx %.12lf (dx_adjust %.12lf)\n", a, b, N, dx, dx_adjust);
-
 
   // tworzenie struktur danych do obsługi wielowątkowości
   int indeksy[l_w]; 
@@ -45,7 +43,6 @@ double calka_zrownoleglenie_petli(double a, double b, double dx, int l_w){
   // oczekiwanie na zakończenie pracy wątków
   for(int i=0; i<l_w; i++ ) pthread_join( watki[i], NULL );
 
-
   return(calka_global);
 }
 
@@ -54,10 +51,10 @@ void* calka_fragment_petli_w(void* arg_wsk){
   int my_id;
   my_id = *( (int *) arg_wsk ); 
 
-  double a, b, dx; // skąd pobrać dane a, b, dx, N, l_w ? 
-  int N, l_w;      // wariant 1 - globalne
+  double a, b, dx;
+  int N, l_w;      
 
-  a = a_global; // itd. itp. - wartości globalne nadaje calka_zrownoleglenie_petli
+  a = a_global; 
   b = b_global;
   dx = dx_global;
   l_w = l_w_global;
@@ -68,32 +65,18 @@ void* calka_fragment_petli_w(void* arg_wsk){
   int my_end = N;
   int my_stride = l_w;
 
-  // dekompozycja blokowa
-  //int my_start = my_id*N/l_w;
-  //int my_end = my_id+1;
-  //int my_stride = 0;
-
-  // something else ? (dekompozycja blokowo-cykliczna)
-
   printf("\nWątek %d: my_start %d, my_end %d, my_stride %d\n", 
 	 my_id, my_start, my_end, my_stride);
-
-
   int i;
   double calka = 0.0;
   for(i=my_start; i<my_end; i+=my_stride){
 
     double x1 = a + i*dx;
     calka += 0.5*dx*(funkcja(x1)+funkcja(x1+dx));
-    //printf("i %d, x1 %lf, funkcja(x1) %lf, całka = %.15lf\n", 
-    //	   i, x1, funkcja(x1), calka);
-
   }
 
   pthread_mutex_lock(&mutex);
   calka_global += calka;
   pthread_mutex_unlock(&mutex);
-
-
 }
 
